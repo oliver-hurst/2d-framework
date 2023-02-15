97673da0688079717928ced5e0f55519239864d4
oliver-hurst <h021185l@student.staffs.ac>
2023-02-15 09:51:58 +0000

    start

---
+++ Assets/Scripts/BasicPlatformer/BasicCharacterController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//--------------------------------------------
+/*Basic Character Controller Includes:  
+    - Basic Jumping
+    - Basic grounding with line traces
+    - Basic horizontal movement
+ */
+//--------------------------------------------
+
+public class BasicCharacterController : MonoBehaviour
+{
+    protected bool facingRight = true;
+    protected bool jumped;
+
+    public float speed = 5.0f;
+   //ublic float sprint = 7.0f;
+    public float jumpForce = 1000;
+
+    private float horizInput;
+
+    public Transform groundedCheckStart;
+    public Transform groundedCheckEnd;
+    public bool grounded;
+
+    public Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        //Linecast to our groundcheck gameobject if we hit a layer called "Level" then we're grounded
+        grounded = Physics2D.Linecast(groundedCheckStart.position, groundedCheckEnd.position, 1 << LayerMask.NameToLayer("Level"));
+        Debug.DrawLine(groundedCheckStart.position, groundedCheckEnd.position, Color.red);
+
+        //Get Player input 
+        horizInput = Input.GetAxis("Horizontal");
+        //Move Character
+        rb.velocity = new Vector2(horizInput * speed * Time.fixedDeltaTime, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && grounded == true)
+        {
+            jumped = true;
+            Debug.Log("Should jump");
+        }
+
+        if (jumped == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            Debug.Log("Jumping!");
+
+            jumped = false;
+        }
+
+        // Detect if character sprite needs flipping
+        if (horizInput > 0 && !facingRight)
+        {
+            FlipSprite();
+        }
+        else if (horizInput < 0 && facingRight)
+        {
+            FlipSprite();
+        }
+
+        // put jump function here
+        //  remember jump anamation 
+        //look into anamations and fix thhe idle position
+
+        //put crouch fucnction here
+
+        //add  sprint function  here
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log(" left shift ");
+            speed = 7.0f;
+
+        }
+
+
+
+
+
+
+        //add enemy ai
+    }
+
+    // Flip Character Sprite
+    void FlipSprite()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+}
